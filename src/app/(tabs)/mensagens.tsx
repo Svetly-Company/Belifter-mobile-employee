ad31c37863e5c5a20a2dabbaa21ac314724c02c5
unknown <jdnn2006@gmail.com>
2024-08-06 10:43:34 -0300

    added: Contatos

--- src/app/(tabs)/mensagens.tsx
+++ src/app/(tabs)/mensagens.tsx
@@ -1,13 +1,28 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HeaderHome from "../../components/HeaderHome";
-import { BoxModel } from "../../components/BoxModel";
-import { WeeklyDiary } from "../../components/WeeklyDiary";
+import { ScrollView } from "react-native-gesture-handler";
+import { Contacts } from "../../components/Contacts";
 
 export default function Mensagens(){
   return(
     <SafeAreaView style={{flex: 1}}>
-      
-    </SafeAreaView>
+          <View className="bg-black flex-1">
+            <ScrollView>
+              <HeaderHome/>
+              <View className="flex w-full px-5 mt-2">
+                <Text className="text-white text-[22px] pt-5">Mensagens</Text>
+                <View className="mt-2">
+                  <Contacts name="Name" notify={true}/>
+                  <Contacts name="Name" notify={true}/>
+                  <Contacts name="Name" notify={true}/>
+                  <Contacts name="Name" notify={true}/>
+                  <Contacts name="Name" notify={true}/>
+                  <Contacts name="Name" notify={true}/>
+                </View>
+              </View>
+            </ScrollView>
+          </View>
+      </SafeAreaView>
   )
 }
--- src/app/(tabs)/treino.tsx
+++ src/app/(tabs)/treino.tsx
@@ -1,35 +1,32 @@
-import { Text, View, ScrollView, TouchableHighlight, Image, Animated } from "react-native";
-import { CaretDown, CaretUp } from "phosphor-react-native";
+import { Text, View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HeaderHome  from "../../components/HeaderHome";
-import { WeeklyDiary } from "../../components/WeeklyDiary";
-import { Workouts } from "../../components/Workouts";
-import { motion, useScroll, useMotionValueEvent } from "framer-motion"
-import { useState } from "react";
 import { BoxModelTwo } from "../../components/BoxModelTwo";
 
 
 export default function Treino(){
 
     return(
-        <SafeAreaView style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1}}>
           <View className="bg-black flex-1">
             <ScrollView>
 
               <HeaderHome/>
-              <Text className="text-white text-[22px] pt-5 pl-10">Alunos</Text>
+              <View className="flex w-full px-5">
+                <Text className="text-white text-[22px] pt-5">Alunos</Text>
 
-              <View>
-                <BoxModelTwo title="Name"/>
-              </View>
+                <View>
+                  <BoxModelTwo title="Name"/>
+                </View>
 
-              <View className="mt-10">
-                <BoxModelTwo title="Name"/>
-                <BoxModelTwo title="Name"/>
-                <BoxModelTwo title="Name"/>
-                <BoxModelTwo title="Name"/>
-                <BoxModelTwo title="Name"/>
-                <BoxModelTwo title="Name"/>
+                <View className="mt-10">
+                  <BoxModelTwo title="Name"/>
+                  <BoxModelTwo title="Name"/>
+                  <BoxModelTwo title="Name"/>
+                  <BoxModelTwo title="Name"/>
+                  <BoxModelTwo title="Name"/>
+                  <BoxModelTwo title="Name"/>
+                </View>
               </View>
 
             </ScrollView>
