ceb5d2d38681f0a20d1c793c1c22a0f7f849d12c
unknown <jdnn2006@gmail.com>
2024-08-20 12:35:31 -0300

    message interface finished, but emojis still bug

--- src/app/(tabs)/(chat)/[chatid].tsx
+++ src/app/(tabs)/(chat)/[chatid].tsx
@@ -1,12 +1,14 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useLocalSearchParams } from 'expo-router' 
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { arr } from '../../../classes/WeekDays/WeekDays'
 import { BoxModel } from '../../../components/BoxModel'
-import { Bell, CaretLeft } from 'phosphor-react-native'
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
+import { Bell, CaretLeft, Key, PaperPlaneRight, Smiley } from 'phosphor-react-native'
+import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 import { MessageBox } from '../../../components/MessageBox'
+import EmojiPicker, { EmojiKeyboard, EmojiType } from 'rn-emoji-keyboard'
+import { NativeEventWrapper } from 'react-native-reanimated/lib/typescript/reanimated2/hook/commonTypes'
 
 export default function Chat() {
   const { chatid } = useLocalSearchParams();
@@ -14,7 +16,12 @@ export default function Chat() {
   if(typeof chatid == "string"){
     id = Number(chatid)
   }
+  let userId = 0
+  let mess = [{id:0 , send: 1, message: "rubão brabo"}, {id: 1, send: 0, message: "bombom é mais"}]
   const ar = arr;
+  const [isOpen, setIsOpen] = React.useState<boolean>(false)
+  const [text, setText] = React.useState<string>("")
+  const [key, setKey] = React.useState<string>("")
   return (
     <SafeAreaView style={{flex: 1}}>
       <View className="bg-gray-950 flex-1">
@@ -35,11 +42,28 @@ export default function Chat() {
           </View>
         </View>
         <ScrollView className="flex w-full flex-col">
-          <MessageBox idSend={1} idUser={0} message='rubão brabo'/>
-          <MessageBox idSend={0} idUser={0} message='bombom é mais'/>
+          {mess.map((x)=>(
+            <MessageBox idSend={x.send} idUser={userId} message={x.message} key={x.id}/>
+          ))}
         </ScrollView>
-        <View className='h-20 w-full bg-gray-950'>
-          
+        <View className='flex flex-row items-center h-20 w-full bg-gray-950'>
+          <View className="flex flex-row w-5/6 mx-2 px-2 py-1 gap-1 items-center border border-white rounded-full">
+                <TextInput id='susamongus' className="flex-1 text-lg color-white px-1" placeholderTextColor={"white"} onKeyPress={(event) => { 
+                    setKey(event.nativeEvent.key ?? "") 
+                    setText(text + key)
+                }} value={text} placeholder="Digite aqui...">{}</TextInput>
+                <TouchableOpacity onPress={() => setIsOpen(true)}>
+                  <Smiley color='white' size={29}/>
+                  <EmojiPicker onEmojiSelected={(EmojiObject: EmojiType) => {setText(text + EmojiObject.emoji)}} open={isOpen} onClose={() => {setIsOpen(false)}} />
+                </TouchableOpacity>
+          </View>
+          <TouchableOpacity>
+            <View className="flex justify-center items-center bg-red-550 h-11 w-11 rounded-full">
+              <TouchableOpacity onPress={() => setText("")}>
+                <PaperPlaneRight weight='fill' color="white" size={24}/>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
